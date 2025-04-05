423850b6b533f259a77ea68c03ea1685d395dfb6
Takshay009 <takshaybhalodiya183@gmail.com>
2025-04-05 10:44:31 +0530

    Update Hotel_Management.cpp
    base program or prototype of hotel management

--- Hotel_Management.cpp
+++ Hotel_Management.cpp
@@ -1,1 +1,52 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// for room
+class Room {
+   
+};
+
+// for hotel system
+class Hotel {
+
+};
+
+
+int main() {
+    
+    
+    do {
+        cout << "\nHotel Booking System\n";
+        cout << "1. Display all rooms\n";
+        cout << "2. Book a room\n";
+        cout << "3. Cancel a booking\n";
+        cout << "4. Modify a booking\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice (1-5): ";
+        cin >> choice;
+        
+        switch (choice) {
+            case 1:
+                hotel.displayRooms();
+                break;
+            case 2:
+                hotel.bookRoom();
+                break;
+            case 3:
+                hotel.cancelBooking();
+                break;
+            case 4:
+                hotel.modifyBooking();
+                break;
+            case 5:
+                cout << "Exiting the system.\n";
+                break;
+            default:
+                cout << "Invalid choice. Please try again.\n";
+        }
+    } while (choice != 5);
+    
+    return 0;
+}
 
